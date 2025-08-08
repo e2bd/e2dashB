c87e9d0ddc0e445fc473790a2f21540c9067d51d
e2bd <e2betbd@gmail.com>
2025-08-08 14:05:07 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -86,14 +86,15 @@ def get_malaysia_time():
 
 def create_driver():
     try:
+        
         chrome_options = Options()
+        chrome_options.binary_location = "/usr/bin/google-chrome-stable"
         # headless + container flags
         chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--window-size=1920,1080")
         chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.binary_location = "/usr/bin/google-chrome-stable"
         # stealth
         chrome_options.add_argument("--disable-blink-features=AutomationControlled")
         chrome_options.add_argument(
