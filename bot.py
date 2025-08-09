c2d6456b1325626f08fb2fe4c1a0896b9815946c
e2bd <e2betbd@gmail.com>
2025-08-09 11:58:29 +0700

    Update bot.py (added jsonify)

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ import supabase
 import os
 from dotenv import load_dotenv
 from pathlib import Path
-from flask import Flask, Response, render_template, make_response
+from flask import Flask, Response, render_template, make_response, jsonify
 from datetime import datetime, timezone, timedelta
 
 
